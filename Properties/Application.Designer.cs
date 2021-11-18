fa5f46f22930b3d663ab2eee33de274154132a1f
ramismonoshev <49976559+ramismonoshev@users.noreply.github.com>
2021-11-18 13:32:59 +0600

    ramismonoshev

---
+++ Properties/Application.Designer.cs
@@ -0,0 +1,46 @@
+using System.Diagnostics;
+using System;
+using System.Xml.Linq;
+using System.Windows.Forms;
+using System.Collections;
+using System.Drawing;
+using Microsoft.VisualBasic;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+
+
+
+namespace Grading_System
+{
+	namespace My
+	{
+		
+		
+		public partial class MyApplication : global::Microsoft.VisualBasic.ApplicationServices.WindowsFormsApplicationBase
+		{
+			[STAThread]
+			static void Main()
+			{
+				(new MyApplication()).Run(new string[] {});
+			}
+			
+			[global::System.Diagnostics.DebuggerStepThrough()]public MyApplication() : base(global::Microsoft.VisualBasic.ApplicationServices.AuthenticationMode.Windows)
+			{
+				
+				
+				
+				this.IsSingleInstance = false;
+				this.EnableVisualStyles = true;
+				this.SaveMySettingsOnExit = true;
+				this.ShutdownStyle = global::Microsoft.VisualBasic.ApplicationServices.ShutdownMode.AfterMainFormCloses;
+			}
+			
+			[global::System.Diagnostics.DebuggerStepThroughAttribute()]protected override void OnCreateMainForm()
+			{
+				this.MainForm = global::Grading_System.Form1.Default;
+			}
+		}
+	}
+	
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using System;
+using System.Xml.Linq;
+using System.Windows.Forms;
+using System.Collections;
+using System.Drawing;
+using Microsoft.VisualBasic;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+
+
+
+[assembly:AssemblyTitle("gradsys")]
+[assembly:AssemblyDescription("")]
+[assembly:AssemblyCompany("")]
+[assembly:AssemblyProduct("gradsys")]
+[assembly:AssemblyCopyright("Copyright ©  2011")]
+[assembly:AssemblyTrademark("")]
+
+[assembly:ComVisible(false)]
+
+[assembly:Guid("44552293-bc6a-4a70-83d0-685d82902ecf")]
+
+[assembly:AssemblyVersion("1.0.0.0")]
+[assembly:AssemblyFileVersion("1.0.0.0")]
+
